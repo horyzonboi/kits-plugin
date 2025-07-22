024799a8e7f868d9721631c6c15d7e1ed25e62e3
horyzon <mainforchat@gmail.com>
2025-07-23 03:06:46 +0500

    Console + CommandBlock Support For Kits Not TESTED

--- src/main/java/net/horyzon/cmds/RandomCommand.java
+++ src/main/java/net/horyzon/cmds/RandomCommand.java
@@ -1,12 +1,16 @@
 package net.horyzon.cmds;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.util.io.BukkitObjectInputStream;
 import org.jetbrains.annotations.NotNull;
 
 import java.rmi.MarshalException;
@@ -17,15 +21,19 @@ public class RandomCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] args) {
+        List<ItemStack> netheriteSet = new ArrayList<>();
+        netheriteSet.add(new ItemStack(Material.NETHERITE_HELMET));
+        netheriteSet.add(new ItemStack(Material.NETHERITE_CHESTPLATE));
+        netheriteSet.add(new ItemStack(Material.NETHERITE_LEGGINGS));
+        netheriteSet.add(new ItemStack(Material.NETHERITE_BOOTS));
+        List<ItemStack> diamondSet = new ArrayList<>();
+        diamondSet.add(new ItemStack(Material.DIAMOND_HELMET));
+        diamondSet.add(new ItemStack(Material.DIAMOND_CHESTPLATE));
+        diamondSet.add(new ItemStack(Material.DIAMOND_LEGGINGS));
+        diamondSet.add(new ItemStack(Material.DIAMOND_BOOTS));
+
         if (commandSender instanceof Player) {
             Player player = (Player)commandSender;
-
-            List<ItemStack> netheriteSet = new ArrayList<>();
-            netheriteSet.add(new ItemStack(Material.NETHERITE_HELMET));
-            netheriteSet.add(new ItemStack(Material.NETHERITE_CHESTPLATE));
-            netheriteSet.add(new ItemStack(Material.NETHERITE_LEGGINGS));
-            netheriteSet.add(new ItemStack(Material.NETHERITE_BOOTS));
-
 
             if (args.length == 0) {
                 commandSender.sendMessage("Please state a kit: Netherite | Diamond");
@@ -36,7 +44,7 @@ public class RandomCommand implements CommandExecutor {
                     player.getInventory().setLeggings(netheriteSet.get(2));
                     player.getInventory().setBoots(netheriteSet.get(3));
                 } else if (args[0].equals("diamond")) {     //diamond kit
-                    player.getInventory().addItem();
+                    player.getInventory().setHelmet(diamondSet.get(0));
 
                 } else {
                     return false;
@@ -44,10 +52,34 @@ public class RandomCommand implements CommandExecutor {
             } else {
                 return false;
             }
-
-
         } else {
-            System.out.println("Womp Womp LIl BRo - ok seriously consoles like cant run ts");
+            String playerName = args[1];
+            Player player = Bukkit.getPlayerExact(playerName);
+            if (args.length == 2) {
+                if (player != null && player.isOnline()) {
+                    if (args[0].equalsIgnoreCase("Netherite")) {
+                        player.getInventory().setHelmet(netheriteSet.get(0));
+                        player.getInventory().setChestplate(netheriteSet.get(1));
+                        player.getInventory().setLeggings(netheriteSet.get(2));
+                        player.getInventory().setBoots(netheriteSet.get(3));
+                    } else if (args[0].equalsIgnoreCase("Diamond")) {
+                        player.getInventory().setHelmet(diamondSet.get(0));
+                        player.getInventory().setChestplate(diamondSet.get(1));
+
+                    } else {
+                        commandSender.sendMessage("Only valid kits are <Diamond | Netherite");
+                    }
+                } else {
+                    commandSender.sendMessage("Player either Offline or Inaccessible ");
+                }
+            } else {
+                if (commandSender instanceof ConsoleCommandSender) {
+                    commandSender.sendMessage(ChatColor.ITALIC + "In order to execute through console, use the following parameters!");
+                    commandSender.sendMessage("/kit <Netherite | Diamond> <PlayerName>");
+                } else {
+                    commandSender.sendMessage(ChatColor.RED + "To execute: /kit <Netherite | Diamond> <PlayerName>");
+                }
+            }
         }
         return true;
     }
